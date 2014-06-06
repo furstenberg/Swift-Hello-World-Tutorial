9f6380c26db76b6261e20c291c25007fc0e572f8
Michael Fürstenberg <michael@furstenberg.co>
2014-06-06 23:00:59 +0200

    [Change] Added a button and a label and pressing the button toggles the display of the label.

--- Hello World Tutorial/ViewController.swift
+++ Hello World Tutorial/ViewController.swift
@@ -9,17 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet var label1 : UILabel
+    @IBOutlet var button1 : UIButton
+    
                             
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func action(sender : AnyObject) {
+        toggleLabelVisibility()
+        toggleButtonText()
     }
-
-
+    
+    func toggleLabelVisibility(){
+        label1.hidden = !label1.hidden
+    }
+    
+    func toggleButtonText(){
+        if(button1.currentTitle == "Hide Label")
+        {
+            button1.setTitle("Show Label", forState:UIControlState.Normal)
+        }
+        else
+        {
+             button1.setTitle("Hide Label", forState:UIControlState.Normal)
+        }
+    }
 }
 
